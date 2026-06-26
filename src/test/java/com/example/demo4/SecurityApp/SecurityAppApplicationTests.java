5db3f980735c9d771ac4ae738e0324788009666a
Ishaan Gupta <ishaanguptast21904@gmail.com>
2026-06-26 14:38:08 +0530

    Adding JWTtoken Service Responsible for generating a new JWT Token and Verifying the JwtToken

--- src/test/java/com/example/demo4/SecurityApp/SecurityAppApplicationTests.java
+++ src/test/java/com/example/demo4/SecurityApp/SecurityAppApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo4.SecurityApp;
 
+import com.example.demo4.SecurityApp.entities.User;
+import com.example.demo4.SecurityApp.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityAppApplicationTests {
 
+	@Autowired
+	private JwtService jwtService;
 	@Test
 	void contextLoads() {
+		User user = new User(4L,"Ishaan@gmail.com","Ishaan12345");
+
+		String jwtToken = jwtService.generateToken(user);
+		System.out.println(jwtToken);
+
+		Long id = jwtService.getUserIdFromToken(jwtToken);
+		System.out.println(id);
 	}
 
 }
