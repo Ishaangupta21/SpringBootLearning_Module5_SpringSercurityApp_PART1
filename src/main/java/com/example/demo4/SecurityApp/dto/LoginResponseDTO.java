b4da760b7a56d0f4c1d7c2606313ab114eb78238
Ishaan Gupta <ishaanguptast21904@gmail.com>
2026-07-01 19:09:02 +0530

    Using Refresh Token and Implementing them

---
+++ src/main/java/com/example/demo4/SecurityApp/dto/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo4.SecurityApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private Long id;
+    private String accessToken;
+    private String refreshToken;
+}
